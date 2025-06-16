c09d8d7e53dbefd8fc60c212a105e2009b17a44f
Dylan Mascarenhas <dylanmascarenhas13@gmail.com>
2025-06-16 14:08:45 +0530

    woah

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,17 @@
-import { motion } from "framer-motion";
 import Navbar from "../components/Navbar";
 import Landing from "../components/Landing";
 import Features from "../components/Features";
+import CallToAction from "../components/CallToAction"
 import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className="font-sans bg-primary-dark min-h-screen">
+    <div className="min-h-screen">
       <Navbar />
       <main>
         <Landing />
         <Features />
+        <CallToAction />
       </main>
       <Footer />
     </div>
