c876c030a225b611d2b7011d7e0f0e9ca050ac45
Dylan Mascarenhas <dylanmascarenhas13@gmail.com>
2025-06-21 19:28:21 +0530

    changed homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import Navbar from "../components/NavBar";
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
 import Landing from "../components/Landing";
 import Features from "../components/Features";
-import CallToAction from "../components/CallToAction"
+import CallToAction from "../components/CallToAction";
 import Footer from "../components/Footer";
-import React, { useState, useEffect } from "react";
+import ColorPicker from "../components/ColorPicker";
+import { getComplementaryColor } from '../utils/colors';
 
-import { getComplementaryColor } from '../utils/colors'; 
 export default function Home() {
   const [theme, setTheme] = useState({
     name: 'Ocean',
@@ -14,11 +15,34 @@ export default function Home() {
     tertiary: '#4ECDC4',
     accent: '#FF6B6B'
   });
+  
+  const [colorPickerOpen, setColorPickerOpen] = useState(false);
+
+  const handleThemeChange = (newTheme) => {
+    setTheme(newTheme);
+  };
 
   return (
-    <div>
-      <Navbar theme={theme} onColorPickerOpen={() => setColorPickerOpen(true)} />
-  <Landing theme={theme} />
+    <div className="relative min-h-screen">
+      <Navbar 
+        theme={theme} 
+        onColorPickerOpen={() => setColorPickerOpen(true)} 
+      />
+      
+      <Landing theme={theme} />
+      
+      <Features theme={theme} />
+      
+      <CallToAction theme={theme} />
+      
+      <Footer theme={theme} />
+      
+      <ColorPicker
+        theme={theme}
+        onThemeChange={handleThemeChange}
+        isOpen={colorPickerOpen}
+        onClose={() => setColorPickerOpen(false)}
+      />
     </div>
   );
 }
